395d591a3488553dbf82fc3d2034ecc678a3837b
Kevin Nguyen <kevinngx625@gmail.com>
2019-03-20 18:22:07 +1100

    Auto Changes

---
+++ TutorialFive/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+
+    public static void main(String[] args) {
+        String myString = "Hello, World";
+        char[] myCharArray = myString.toCharArray();
+
+        for (int i = 0; i < myCharArray.length; i++) {
+            System.out.println(myCharArray[i]);
+        }
+    }
+}
